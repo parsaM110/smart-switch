42194d82898eb134b9bb3342a66d9d02a253bb1a
parsa momeni <101204725+parsaM110@users.noreply.github.com>
2026-01-27 17:36:45 +0330

    Modify LED states and enhance WiFi handling
    Updated LED states and added WiFi reconnection logic.

--- main.ino
+++ main.ino
@@ -7,23 +7,22 @@
 #include "DHT.h"
 #include <EEPROM.h>
 
+
 const char* websockets_server_host = "http://spadwebsocket.runflare.run/ws/chat/lobby_room/"; //Enter server adress
 
 
 using namespace websockets;
 
-unsigned long previousMillis = 0;  //will store last time LED was blinked
+unsigned long previousMillis = 0;
+unsigned long previousMillis1 = 0;  //will store last time LED was blinked
+int previousState = 0;
+unsigned long rst_millis ;
 const long period = 6000;
-
-WiFiUDP Udp;
-
-//--------------------------------------------
-#define WiFi_rst 0                  //WiFi credential reset pin (Flash button on ESP8266)
-unsigned long rst_millis;
 //--------------------------------------------
 String ssid;                        //string variable to store ssid
 String pss;                         //string variable to store password
 //-----------------------------------------
+WiFiUDP Udp;
 
 #define BUTTON_PIN1 5  // ESP32 pin GIOP14, which connected to button
 #define BUTTON_PIN2 4  // ESP32 pin GIOP15, which connected to button
@@ -35,10 +34,10 @@ String pss;                         //string variable to store password
 
 
 // The below are variables, which can be changed
-int led_state1 = HIGH;    // the current state of LED
-int led_state2 = HIGH;    // the current state of LED
-int led_state3 = LOW;    // the current state of LED
-int led_state4 = LOW;    // the current state of LED
+int led_state1 = LOW;    // the current state of LED
+int led_state2 = LOW;    // the current state of LED
+int led_state3 = HIGH;    // the current state of LED
+int led_state4 = HIGH;    // the current state of LED
 
 int button_state1;       // the current state of button
 int button_state2;       // the current state of button
@@ -48,7 +47,7 @@ int last_button_state2;  // the previous state of button
 
 WebsocketsClient client;
 
-#define DHTPIN 2     // Digital pin connected to the DHT sensor
+#define DHTPIN 2    // Digital pin connected to the DHT sensor
 
 #define DHTTYPE DHT11   // DHT 11
 
@@ -59,10 +58,9 @@ void setup() {
 
  Serial.begin(115200);               // initialize serial
 
-  //-----------------------------------------
-  pinMode(WiFi_rst, INPUT_PULLUP);
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, HIGH);
+   //-----------------------------------------
+  
+  
   EEPROM.begin(200);
  
 //-----------------------------------------
@@ -73,16 +71,21 @@ void setup() {
 
   pinMode(BUTTON_PIN2, INPUT); // set ESP32 pin to input pull-up mode
   pinMode(LED_PIN2, OUTPUT);          // set ESP32 pin to output mode
-  pinMode(LED_PIN4, OUTPUT);          // set ESP32 pin to output mode
+  pinMode(LED_PIN4, OUTPUT);  
+
+  // set ESP32 pin to output mode
 
   button_state1 = digitalRead(BUTTON_PIN1);
   button_state2 = digitalRead(BUTTON_PIN2);
   
-    digitalWrite(LED_PIN1, HIGH);
-    digitalWrite(LED_PIN2, HIGH);
-    digitalWrite(LED_PIN3, LOW);
-    digitalWrite(LED_PIN4, LOW);
-  
+    digitalWrite(LED_PIN1, LOW);
+    digitalWrite(LED_PIN2, LOW);
+    digitalWrite(LED_PIN3, HIGH);
+    digitalWrite(LED_PIN4, HIGH);
+   
+
+ 
+ 
 
 
    dht.begin();
@@ -108,56 +111,24 @@ void setup() {
     Serial.println(pss);
   
 //-----------------------------------------
-  
+    
     WiFi.begin(ssid.c_str(), pss.c_str());
+    WiFi.setAutoReconnect(true);
+    WiFi.persistent(true);
 
-    delay(5000);
+
+WiFi.mode(WIFI_STA);
+
+   
+
+    
 
 
     //-----------------------------------------
 
-  if (WiFi.status() != WL_CONNECTED) // if WiFi is not connected
-  {
-  
-    WiFi.mode(WIFI_STA);
-    WiFi.beginSmartConfig();
-//------------------------------------------
-  Serial.println("waiting for smartConfig");
-  while(!WiFi.smartConfigDone()) {
-    delay(500);
-    Serial.print(".");
-  }
-//------------------------------------------
-   Serial.println("");
-   Serial.println("SmartConfig Successsssss");
-  
+    
 
-    //WiFi.printDiag(Serial);
-  ssid = WiFi.SSID();
-  pss = WiFi.psk();
-  Serial.print("SSID:");
-  Serial.println(ssid);
-  Serial.print("PSS:");
-  Serial.println(pss);
-  Serial.println("writing eeprom ssid:");
-        for (int i = 0; i < ssid.length(); ++i)
-        {
-          EEPROM.write(i, ssid[i]);
-          Serial.print("Wrote: ");
-          Serial.println(ssid[i]);
-        }
-        EEPROM.write( ssid.length(), '\0');
-        Serial.println("writing eeprom pass:");
-        for (int i = 0; i < pss.length(); ++i)
-        {
-          EEPROM.write(32 + i, pss[i]);
-          Serial.print("Wrote: ");
-          Serial.println(pss[i]);
-        }
-        EEPROM.write(32 + pss.length(), '\0');
-        EEPROM.commit();
  
-
 
 
 
@@ -174,12 +145,15 @@ void loop() {
 
   
    // save the last time you blinked the LED
-   if(client.available()) {
-                   int humid = dht.readHumidity(); 
-                   int temp = dht.readTemperature();
-                   unsigned long currentMillis = millis(); // store the current time
+                  unsigned long currentMillis = millis();
+                 if(client.available()) {
+                
+                   float humid = dht.readHumidity(); 
+                   float temp = dht.readTemperature();
+                    // store the current time
                     if (currentMillis - previousMillis >= period) { // check if 1000ms passed
                      previousMillis = currentMillis;
+                     
                      client.send("temp :" + String(temp));
                      client.send("humid :" + String(humid));
                     }   
@@ -188,28 +162,31 @@ void loop() {
                       client.poll();
                    }
   
-         esptouch();
+        esptouch();
          Udp.parsePacket();
 
-
-
-  rst_millis = millis();
-  while (digitalRead(WiFi_rst) == LOW){
-  
-   }
-  // check the button press time if it is greater than 3sec clear wifi cred and restart ESP 
-  if (millis() - rst_millis >= 3000){
-  digitalWrite(LED_BUILTIN, LOW);
+        
+       
+       if( digitalRead(4)== HIGH){
+        
+        previousMillis1=millis();
+       }
+       
+           
+       if ( digitalRead(4)== LOW ) {
+            if (currentMillis - previousMillis1 >= 9000){
+         
+          
   for (int i = 0; i < 96; ++i)
         {
           EEPROM.write(i, 0);
         }
   EEPROM.commit();
-  delay(5000);
-  ESP.restart();
-  }
-        
 
+  ESP.restart();
+          
+         } 
+       }
 
 
           
@@ -242,9 +219,9 @@ void loop() {
 
     
   }
+   
   if (last_button_state2 == HIGH && button_state2 == LOW) {
-    Serial.println("The button2 is pressed");
-
+   
     // toggle state of LED
     led_state2 = !led_state2;
     led_state4 = !led_state2;
@@ -279,6 +256,33 @@ void esptouch(){
              // Start the server
                  Udp.begin(49999);
                  websocketfunction();
+
+
+
+                  //WiFi.printDiag(Serial);
+  ssid = WiFi.SSID();
+  pss = WiFi.psk();
+  Serial.print("SSID:");
+  Serial.println(ssid);
+  Serial.print("PSS:");
+  Serial.println(pss);
+  Serial.println("writing eeprom ssid:");
+        for (int i = 0; i < ssid.length(); ++i)
+        {
+          EEPROM.write(i, ssid[i]);
+          Serial.print("Wrote: ");
+          Serial.println(ssid[i]);
+        }
+        EEPROM.write( ssid.length(), '\0');
+        Serial.println("writing eeprom pass:");
+        for (int i = 0; i < pss.length(); ++i)
+        {
+          EEPROM.write(32 + i, pss[i]);
+          Serial.print("Wrote: ");
+          Serial.println(pss[i]);
+        }
+        EEPROM.write(32 + pss.length(), '\0');
+        EEPROM.commit();
                 
                  
              }
